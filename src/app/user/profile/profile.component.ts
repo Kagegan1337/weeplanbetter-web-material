08092ff4c158dad8e901afb4b4da2c7179b77a41
Jetzlaff <jetzlaff@oktopos.com>
2024-05-12 13:32:58 +0200

    add profile

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {AsyncPipe, NgIf} from "@angular/common";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {map} from "rxjs";
+import {AuthserviceService} from "../../service/authservice.service";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    NgIf,
+    AsyncPipe,
+
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
 
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthserviceService) {
+  }
+
+  isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+
+  username() {
+    return this.authService.getUsername();
+  }
 }
