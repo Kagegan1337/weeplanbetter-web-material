5d4a281de196eaa7d2ef2eb81db26f63c7e17fe5
Kagegan <kagegan1337@gmail.com>
2024-08-04 14:42:33 +0200

    added profile dto

---
+++ src/app/model/dto/user/profil-dto.ts
@@ -0,0 +1,10 @@
+export interface ProfilDto {
+  firstName: string;
+  lastName: string;
+  birth: string;
+  email: string;
+  business: boolean;
+  verified: boolean;
+  since: string;
+  role: number;
+}
