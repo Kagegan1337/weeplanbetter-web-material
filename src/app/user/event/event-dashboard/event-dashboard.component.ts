b1573da4db0f00bf26c89e86f61700d7059da41e
Kagegan <kagegan1337@gmail.com>
2024-08-06 17:20:18 +0200

    add dashboard dradanddrop

---
+++ src/app/user/event/event-dashboard/event-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-dashboard.component.html',
+  styleUrl: './event-dashboard.component.scss'
+})
+export class EventDashboardComponent {
+
+}
