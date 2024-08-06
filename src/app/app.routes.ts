4b62ce812742187a9549366fb70e32e065d1c21f
Kagegan <kagegan1337@gmail.com>
2024-08-06 17:19:54 +0200

    add dashboard dradanddrop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,17 @@ import {DashboardComponent} from "./user/dashboard/dashboard.component";
 import {LoginComponent} from "./main/menu/login/login.component";
 import {authGuardGuard} from "./guards/auth-guard.guard";
 import {ProfileComponent} from "./user/profile/profile.component";
-import {EventOverviewComponent} from "./event/event-overview/event-overview.component";
 import {SignInComponent} from "./main/menu/sign-in/sign-in.component";
 import {SuccessComponent} from "./dialogs/success/success.component";
+import {EventDashboardComponent} from "./user/event/event-dashboard/event-dashboard.component";
+import {GroupDashboardComponent} from "./user/group/group-dashboard/group-dashboard.component";
 
 export const routes: Routes = [
 
   {path: "dashboard", component: DashboardComponent, canActivate: [authGuardGuard]},
   {path: "profile", component: ProfileComponent, canActivate: [authGuardGuard]},
-  {path: "events", component: EventOverviewComponent, canActivate: [authGuardGuard]},
+  {path: "events", component: EventDashboardComponent, canActivate: [authGuardGuard]},
+  {path: "groups", component: GroupDashboardComponent, canActivate: [authGuardGuard]},
   {path: "login", component: LoginComponent},
   {path: "signup", component: SignInComponent},
   {path: "success", component: SuccessComponent}
--- src/app/user/dashboard/dashboard.component.ts
+++ src/app/user/dashboard/dashboard.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardModule} from "@angular/material/card";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {Router} from "@angular/router";
+import {MatSlideToggle, MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup, moveItemInArray} from "@angular/cdk/drag-drop";
+import {MatDatepicker, MatDatepickerModule} from "@angular/material/datepicker";
+import {NgForOf, NgSwitch, NgSwitchCase} from "@angular/common";
+import {MatList, MatListModule} from "@angular/material/list";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    CdkDropListGroup,
+    MatDatepickerModule,
+    CdkDropList,
+    NgSwitch,
+    MatListModule,
+    NgForOf,
+    CdkDrag,
+    NgSwitchCase,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  editMode: boolean = false;
 
+  items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
+
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+  }
 }
